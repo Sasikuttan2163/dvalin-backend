acebfba80ab4434cb355d11331e6db2c05ac16d9
Sasikuttan2163 <abhinav.anil.2k5@gmail.com>
2024-09-14 13:21:48 +0530

    chore: not sure what happened

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -251,6 +251,9 @@ class DataService {
 		return tempIndex;
 	}
 
+	// Congratulations Traveler on making it this far! I have a small reward for you:
+	// https://www.youtube.com/watch?v=dz1MhkbPthI enjoy :)
+
 	public getBanner = (): BannerData => {
 		return this.bannerData;
 	};
